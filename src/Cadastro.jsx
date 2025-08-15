8ff738ee913112fd20e6392c15e5efbfd3229b78
RianSantos09 <riansantosn45@gmail.com>
2025-08-15 11:54:59 -0300

    add css global e pasta pages
    adicionando css a todas as paginas e pasta separada paginas

---
+++ src/Cadastro.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './cadastro.css'
+import './index.css'
+
+function Cadastro() {
+    return(
+    <div>
+        <h1>cadastro</h1>
+    </div>
+    )
+}
+  
+export default Cadastro
